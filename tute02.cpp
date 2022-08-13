51e1df9cddc43d03790f350682b9b1a5e41aac63
it21054518 <110513758+IT21054518@users.noreply.github.com>
2022-08-13 11:02:05 +0000

    tutorial 2 tute 1 The given C programs are converted to C++

--- tute02.cpp
+++ tute02.cpp
@@ -12,7 +12,7 @@ Type = 3
 OtRate = 1700
 Please Note that the input command in C++ is std::cin. This is a representation of the Keyboard.*/
 
-#include <stdio.h>
+/*#include <stdio.h>
 int main()
 {
    double salary, netSalary;
@@ -41,4 +41,22 @@ int main()
    printf("Net Salary is %f ", netSalary);
   
    return 0;
+}*/
+#include<iostream>
+int main()
+{
+    int no;
+    long fac;
+    int r;
+
+    std :: cout << "Enter a Number : ";
+    std :: cin >> no;
+
+    fac = 1;
+    for (r=no; r >= 1; r--) {
+        fac = fac * r;
+    }
+
+    std :: cout << "Factorial of "<< no << " is "<< fac << "\n";
+    return 0;
 }
